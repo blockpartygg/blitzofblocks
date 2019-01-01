ba2efa912afa651c8ba0503782d2b982c867b6cd
Ron Forbes <ron@ronforbes.me>
2018-12-31 16:23:45 -0800

    UpdatedCursorRenderer with public visibility setter

--- Assets/Scripts/CursorRenderer.cs
+++ Assets/Scripts/CursorRenderer.cs
@@ -4,9 +4,15 @@ using System;
 public class CursorRenderer : MonoBehaviour {
     [SerializeField] Cursor cursor = null;
     [SerializeField] Transform Root = null;
+    [SerializeField] SpriteRenderer spriteRenderer = null;
     Vector3 cursorTranslation;
+
     void Start() {
         cursorTranslation = Vector3.zero;
+    }
+
+    public void SetVisible(bool isVisible) {
+        spriteRenderer.enabled = isVisible;
     }
 
     void Update() {
