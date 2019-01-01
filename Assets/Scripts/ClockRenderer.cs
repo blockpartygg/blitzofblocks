3d42275bb2b0914b3e02617d22a9da5b51686354
Ron Forbes <ron@ronforbes.me>
2018-12-31 16:23:10 -0800

    Updated ClockRenderer to only update once per second

--- Assets/Scripts/ClockRenderer.cs
+++ Assets/Scripts/ClockRenderer.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
 using TMPro;
 using System;
+using System.Collections;
 
 public class ClockRenderer : MonoBehaviour {
 	[SerializeField] ClockManager clockManager = null;
 	[SerializeField] TMP_Text text = null;
 	[SerializeField] StringReference stringFormat = null;
 
-	void Update() {
-		TimeSpan timeRemaining = TimeSpan.FromSeconds(clockManager.SecondsRemaining);
-		
-		text.text = string.Format(stringFormat.Value, timeRemaining.Minutes, timeRemaining.Seconds, timeRemaining.Milliseconds);
+	void Start() {
+		StartCoroutine(UpdateTimeRemaining());
+	}
+
+	IEnumerator UpdateTimeRemaining() {
+		while(true) {
+			TimeSpan timeRemaining = TimeSpan.FromSeconds(clockManager.SecondsRemaining);
+			text.text = string.Format(stringFormat.Value, timeRemaining.Minutes, timeRemaining.Seconds);
+			yield return new WaitForSeconds(1f);
+		}
 	}
 }
