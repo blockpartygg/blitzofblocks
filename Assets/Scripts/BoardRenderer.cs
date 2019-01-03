60400015926ae366d9cea0efabdeb5afc35c77a7
Ron Forbes <ron@ronforbes.me>
2019-01-02 18:15:40 -0800

    Converted board to sprite and removed cube geometry

--- Assets/Scripts/BoardRenderer.cs
+++ Assets/Scripts/BoardRenderer.cs
@@ -1,11 +1,8 @@
 using UnityEngine;
 
 public class BoardRenderer : MonoBehaviour {
-    [SerializeField] Transform backWallTransform = null;
-    [SerializeField] Transform leftWallTransform = null;
-    [SerializeField] Transform rightWallTransform = null;
-    [SerializeField] Transform topWallTransform = null;
-    [SerializeField] Transform bottomWallTransform = null;
+    [SerializeField] Transform spriteTransform = null;
+    [SerializeField] Transform spriteMaskTransform = null;
     [SerializeField] Transform blockRoot = null;
     [SerializeField] Transform cursorRoot = null;
     [SerializeField] Transform panelRoot = null;
@@ -13,16 +10,11 @@ public class BoardRenderer : MonoBehaviour {
     [SerializeField] IntReference boardRows = null;
 
     void Start() {
-        backWallTransform.position = new Vector3(0, 0.5f * boardRows.Value, 0.5f);
-        backWallTransform.localScale = new Vector3(boardColumns.Value, boardRows.Value, 0.1f);
-        leftWallTransform.position = new Vector3(-0.5f * boardColumns.Value, 0.5f * boardRows.Value, -0.5f);
-        leftWallTransform.localScale = new Vector3(0.1f, boardRows.Value, 2f);
-        rightWallTransform.position = new Vector3(0.5f * boardColumns.Value, 0.5f * boardRows.Value, -0.5f);
-        rightWallTransform.localScale = new Vector3(0.1f, boardRows.Value, 2f);
-        topWallTransform.position = new Vector3(0, boardRows.Value, -0.5f);
-        topWallTransform.localScale = new Vector3(boardColumns.Value, 0.1f, 2f);
-        bottomWallTransform.position = new Vector3(0, 0, -0.5f);
-        bottomWallTransform.localScale = new Vector3(boardColumns.Value, 0.1f, 2f);
+        spriteTransform.position = new Vector3(0, 0.5f * boardRows.Value, 0.5f);
+        spriteTransform.localScale = new Vector3(boardColumns.Value, boardRows.Value, 1f);
+
+        spriteMaskTransform.position = new Vector3(0, 0.5f * boardRows.Value, 0.5f);
+        spriteMaskTransform.localScale = new Vector3(boardColumns.Value, boardRows.Value, 1f);
 
         blockRoot.position = new Vector3(-0.5f * (boardColumns.Value - 1), 0, 0);
         cursorRoot.position = new Vector3(Mathf.Ceil(-0.5f * (boardColumns.Value - 1)), 0.5f, -0.1f);
