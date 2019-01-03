70f98e7996eba11f1556d96d2d3c2ae1cbd3c586
Ron Forbes <ron@ronforbes.me>
2019-01-02 18:15:23 -0800

    Added tweening and mode objective announcement

--- Assets/Scripts/AnnouncementManager.cs
+++ Assets/Scripts/AnnouncementManager.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using DG.Tweening;
 
 public class AnnouncementManager : MonoBehaviour {
     [SerializeField] TMP_Text text = null;
-    WaitForSeconds secondWait;
+    WaitForSeconds oneSecondWait;
+    WaitForSeconds threeSecondWait;
     
     void Awake() {
-        secondWait = new WaitForSeconds(1f);
+        oneSecondWait = new WaitForSeconds(1f);
+        threeSecondWait = new WaitForSeconds(3f);
     }
 
     public void ShowGameStarting() {
@@ -17,27 +20,41 @@ public class AnnouncementManager : MonoBehaviour {
     IEnumerator RunCountdownSequence() {
         text.enabled = true;
 
+        yield return StartCoroutine(RunModeObjectiveCoroutine());
         yield return StartCoroutine(RunThreeCoroutine());
         yield return StartCoroutine(RunTwoCoroutine());
         yield return StartCoroutine(RunOneCoroutine());
     }
 
+    IEnumerator RunModeObjectiveCoroutine() {
+        text.text = "Time Attack\n<size=64>Score as many points in 2 minutes as possible</size>";
+        text.transform.DOScale(0, 0.25f).SetDelay(2);
+
+        yield return threeSecondWait;
+    }
+
     IEnumerator RunThreeCoroutine() {
         text.text = "3";
+        text.transform.DOScale(1, 0.25f);
+        text.transform.DOScale(0, 0.25f).SetDelay(0.5f);
 
-        yield return secondWait;
+        yield return oneSecondWait;
     }
 
     IEnumerator RunTwoCoroutine() {
         text.text = "2";
+        text.transform.DOScale(1, 0.25f);
+        text.transform.DOScale(0, 0.25f).SetDelay(0.5f);
 
-        yield return secondWait;
+        yield return oneSecondWait;
     }
 
     IEnumerator RunOneCoroutine() {
         text.text = "1";
+        text.transform.DOScale(1, 0.25f);
+        text.transform.DOScale(0, 0.25f).SetDelay(0.5f);
 
-        yield return secondWait;
+        yield return oneSecondWait;
     }
 
     public void ShowGamePlaying() {
@@ -46,8 +63,11 @@ public class AnnouncementManager : MonoBehaviour {
 
     IEnumerator RunGoCoroutine() {
         text.text = "Go!";
+        text.transform.DOScale(2, 0.25f);
+        text.transform.DOScale(0, 0.75f).SetDelay(1f);
+        text.DOColor(Color.clear, 0.75f).SetDelay(1f);
 
-        yield return secondWait;
+        yield return new WaitForSeconds(2f);
 
         text.enabled = false;
     }
@@ -55,5 +75,7 @@ public class AnnouncementManager : MonoBehaviour {
     public void ShowGameEnding() {
         text.enabled = true;
         text.text = "Time's up!";
+        text.color = Color.white;
+        text.transform.DOScale(2, 0.25f * Time.timeScale);
     }
 }
