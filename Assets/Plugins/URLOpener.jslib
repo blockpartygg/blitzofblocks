974518b701c05fe3c394f195c2f4d2ea6ef696a0
Ron Forbes <ron@ronforbes.me>
2019-01-14 19:12:41 -0800

    Updated URL opener to open in new tab

---
+++ Assets/Plugins/URLOpener.jslib
@@ -0,0 +1,5 @@
+mergeInto(LibraryManager.library, {
+    OpenURL: function(url) {
+        window.open(url);
+    }
+});
--- Assets/Scripts/URLOpener.cs
+++ Assets/Scripts/URLOpener.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class URLOpener : MonoBehaviour {
+    [DllImport("__Internal")] static extern void OpenURL(string url);
+
     public void Open(string url) {
+#if !UNITY_EDITOR && UNITY_WEBGL
+        OpenURL(url);
+#else
         Application.OpenURL(url);
+#endif
     }
 }
